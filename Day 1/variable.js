006932da3bdb971cc8ff4bbe9b7c813ee32283b8
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2023-08-08 12:34:18 +0530

    Initial Commit

---
+++ Day 1/variable.js
@@ -0,0 +1,16 @@
+let age=23;
+let year=2023;
+
+console.log(age,year);
+
+age = 30;
+console.log(age);
+
+const point = 100;
+console.log(point);
+
+var score = 50;
+console.log(score);
+
+score = 25;
+console.log(score);
