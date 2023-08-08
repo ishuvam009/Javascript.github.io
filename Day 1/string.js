cd1d04cf65065832e64221cf2e25153667d83c34
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2023-08-08 12:56:05 +0530

    Initial Commit

---
+++ Day 1/string.js
@@ -0,0 +1,31 @@
+// String
+console.log('Hello');
+
+let email = 'shuvam@gmail.com';
+console.log(email);
+
+
+// String Concartination
+let firstName = 'Shuvam';
+let lastName = 'Mandal';
+
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+
+// Getting Characters
+console.log(firstName[0]);
+
+// String Length
+console.log(fullName.length);
+
+// Sting Methods
+console.log(fullName.toUpperCase());  //toUpperCase converts all the text to uppercase.
+
+let result = fullName.toLocaleLowerCase();
+console.log(result);
+
+let index = email.indexOf('@');
+
+console.log(index);
