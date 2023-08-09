dd9414b7218fd44623a1d7cd335a9978d6894768
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2023-08-10 01:04:49 +0530

    Initial Commit

--- Day 1/Template Stringd.js
+++ Day 1/Template Stringd.js
@@ -6,3 +6,16 @@ const likes = 27;
 
 let result = 'The blog called ' + title + ' by ' + author + ' has '+ likes + ' likes.';
 console.log(result);
+
+// Template String way.
+let results = `The blog called ${title} by ${author} has ${likes} likes`;
+console.log(results);
+
+// Creating html template.
+let html =`
+            <h1>${title}</h1>
+            <p>by ${author}</p>
+            <span>This blog has ${likes} likes.</span>
+`;
+
+console.log(html);
