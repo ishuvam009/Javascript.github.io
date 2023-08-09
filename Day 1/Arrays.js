6bfeaa6dd4a8ae04524064a2c327a4f265e7778b
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2023-08-10 01:32:31 +0530

    Initial Commit

---
+++ Day 1/Arrays.js
@@ -0,0 +1,47 @@
+// Arrays in string.
+
+let ninjas = ['Shuvam', 'Kumar','Mandal'];
+console.log(ninjas);
+
+//Selecting from a position.
+
+console.log(ninjas[2]);
+
+//Update a new item in position 2.
+
+ninjas[2]= 'Ojha';
+
+console.log(ninjas);
+
+// Arrays in numbers.
+
+let ages = [12, 19, 50, 70, 99];
+console.log(ages);
+
+console.log(ages[2]);
+
+// Mixed data types.
+
+let mix = ['HI', 30, 'KIIT', 90];
+console.log(mix);
+
+/// Array size.
+
+console.log(ninjas.length);
+
+// Array menthods.
+
+let newArray = ['Hi','this','is','a','javascript','code'];
+
+let join = newArray.join('-'); //Joinning them with a '-' between them. 
+console.log(join);
+
+//To find the index.
+
+let findTheIndex = newArray.indexOf('javascript');
+console.log(findTheIndex);
+
+// Joining two array.
+
+let joinArray = ninjas.concat(newArray);
+console.log(joinArray);
