8f948304fa1b1f3d64fcbaaa1bf990ae8b9478f2
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2023-08-24 09:22:36 +0530

    Initial Commit

--- Day 3/Functions.js
+++ Day 3/Functions.js
@@ -1,3 +1,21 @@
+
+
+// Calling the function.
+
+add();   // Hoisting works with function declarations but not with function expression.
+
+// Hoisting is JavaScript's default behavior of moving all declarations to the top of the current scope.
+
+
+
+// Function expression.
+
+const speak = function() {
+    console.log('Good Day!');
+};
+
+speak();
+
 // Declearing the function.
 
 function add() {
@@ -8,4 +26,5 @@ function add() {
     console.log(sum);
 }
 
-// Calling the function.
+
+
