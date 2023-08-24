0dfc889b37d7ffff3e85981b19d2f0a2bac26828
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2023-08-24 12:28:29 +0530

    Initial Commit

--- Day 3/Functions.js
+++ Day 3/Functions.js
@@ -28,3 +28,40 @@ function add() {
 
 
 
+
+// Argument and Parameters.
+
+const say = function(name, time) {  // here name and time are the parameters.
+    console.log(`good ${time} ${name}`);
+}
+
+
+
+say('Mario', 'morning');  // Here 'Mario' and 'morning' are the arguments.
+
+
+
+
+// Experiment.
+
+
+const exp = function(time, university) {
+    console.log(`Hello, ${time} welcome to ${university}.`);
+}
+
+
+exp('Good Morning','KiiT University!');
+
+// Passing the arguments in a different way.
+
+const way = function(name = 'shuvam', time = 'night') {
+    console.log(`Good ${time} ${name}`);
+}
+
+way(); // Passing the default value decleared in the parameter.
+
+way('Radha','morning'); // Overwriting the default value by the arguments.
+
+
+// Returning values.
+
